a6d52b5350aa99853e2f2579aaf19f43363c0fa9
Travis Pinson <travispinson@gmail.com>
2021-01-11 14:14:04 -0600

    Full CRUD on comment

---
+++ Gifter/Controllers/CommentController.cs
@@ -0,0 +1,67 @@
+﻿using Gifter.Data;
+using Gifter.Models;
+using Gifter.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly CommentRepository _commentRepository;
+
+        public CommentController(ApplicationDbContext context)
+        {
+            _commentRepository = new CommentRepository(context);
+        }
+
+        public IActionResult Get()
+        {
+            return Ok(_commentRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var comment = _commentRepository.GetById(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            return Ok(comment);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Comment comment)
+        {
+            _commentRepository.Add(comment);
+            return CreatedAtAction("Get", new { id = comment.Id }, comment);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _commentRepository.Delete(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Comment comment)
+        {
+            if (id != comment.Id)
+            {
+                return BadRequest();
+            }
+
+            _commentRepository.Update(comment);
+            return NoContent();
+        }
+
+    }
+}
