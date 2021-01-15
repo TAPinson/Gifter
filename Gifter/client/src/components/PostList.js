e8fb9398395c0650dce19b929c97187b1794c70b
Travis Pinson <travispinson@gmail.com>
2021-01-15 14:58:11 -0600

    implemented auth token to fetch calls

--- Gifter/client/src/components/PostList.js
+++ Gifter/client/src/components/PostList.js
@@ -1,14 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import Post from './Post';
+import { UserProfileContext } from "../providers/UserProfileProvider";
 
 const PostList = () => {
+    const { getToken } = useContext(UserProfileContext);
     const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        fetch('/api/post')
-            .then(res => res.json())
-            .then(data => setPosts(data))
+    // useEffect(() => {
+    //     fetch('/api/post')
+    //         .then(res => res.json())
+    //         .then(data => setPosts(data))
 
+    // }, []);
+
+    useEffect(() => {
+        getToken().then((token) =>
+            fetch('/api/post', {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}` // The token gets added to the Authorization header
+                }
+            })
+                .then(resp => resp.json())
+                .then(setPosts));
     }, []);
 
     return (
